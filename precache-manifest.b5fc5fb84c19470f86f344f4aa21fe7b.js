ddb201a64bcafb46c74503b596222ad5952cbfe0
David Winters <davewinters1987@gmail.com>
2020-05-28 02:02:01 +0100

    Updates

---
+++ precache-manifest.b5fc5fb84c19470f86f344f4aa21fe7b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb08141dc5717a3e7dde55718aee1876",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "c93d2fe64ab56b42a8fb",
+    "url": "/weatherapp/static/css/2.53ba116e.chunk.css"
+  },
+  {
+    "revision": "b594ef2e90f5ca433faa",
+    "url": "/weatherapp/static/css/main.2316d419.chunk.css"
+  },
+  {
+    "revision": "c93d2fe64ab56b42a8fb",
+    "url": "/weatherapp/static/js/2.04a4e774.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.04a4e774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b594ef2e90f5ca433faa",
+    "url": "/weatherapp/static/js/main.3fd1756f.chunk.js"
+  },
+  {
+    "revision": "84182e8d5f8272e7494f",
+    "url": "/weatherapp/static/js/runtime-main.cf992ba9.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/weatherapp/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
